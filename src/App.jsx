f7a485f41d5f10a9e09530aba486b6fb941555c5
Houssam Ouhra <houssamblur04@gmail.com>
2025-09-04 10:20:13 +0100

    refactor(react): replace parent <div> with Fragment

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id='core-concepts'>
@@ -48,7 +48,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
